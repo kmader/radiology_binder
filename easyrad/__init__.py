612bb23fbcc601914ba0ed20bb9ce12f16ab28db
Kevin Mader <kevinmader@gmail.com>
2018-08-27 21:26:43 +0200

    updating __init__, cleaning up coverage

--- easyrad/__init__.py
+++ easyrad/__init__.py
@@ -3,6 +3,8 @@ from . import browsing, viz, dicom
 from .browsing import find_files, show_workspace
 from .dicom import load_dicom
 
-__all__ = (find_files,
-           show_workspace,
-           load_dicom)
+__all__ = (
+    "find_files",
+    "show_workspace",
+    "load_dicom"
+)
